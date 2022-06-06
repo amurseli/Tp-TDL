8cf8ce6943a8433f9a016d98c800a404a61d17f2
MateoF01 <mateofernandez380@gmail.com>
2022-06-06 20:38:08 -0300

    database 1

--- TDLColecciones/app/src/main/java/com/example/tdlcolecciones/MainActivity.kt
+++ TDLColecciones/app/src/main/java/com/example/tdlcolecciones/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.tdlcolecciones
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Button
@@ -9,39 +11,60 @@ import android.widget.ListView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class MainActivity : AppCompatActivity() {
 
     var listOfCollections = mutableListOf<Collection>()
-    lateinit var arrayAdapter : ArrayAdapter<*>
+    lateinit var arrayAdapter: ArrayAdapter<*>
+    private val db = FirebaseFirestore.getInstance()
 
-    private val responseLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ activityResult->
-        if(activityResult.resultCode == RESULT_OK){
-            val newCollection = activityResult.data?.getSerializableExtra("nombre") as Collection
-            listOfCollections.add(newCollection)
+    var email = ""
+
+    private val responseLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { activityResult ->
+            if (activityResult.resultCode == RESULT_OK) {
+                val newCollection =
+                    activityResult.data?.getSerializableExtra("nombre") as Collection
+                listOfCollections.add(newCollection)
+            }
         }
-    }
 
     override fun onResume() {
         arrayAdapter.notifyDataSetChanged()
+        db.collection("users").document(email).set(
+            hashMapOf("COLECCIONES" to listOfCollections)
+        )
         super.onResume()
     }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var email = intent.extras?.get("email")
+        email = intent.extras?.get("email").toString()
 
-        Toast.makeText(this,email.toString(), Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, email, Toast.LENGTH_SHORT).show()
+
+        //db.collection("users").document(email).get().addOnSuccessListener {
+        //    listOfCollections.addAll(it.get("COLECCIONES") as MutableList<Collection>)
+        //}
 
         val fab: View = findViewById(R.id.fab1)
         val list: ListView = findViewById(R.id.list1)
 
-        arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,listOfCollections)
+        arrayAdapter =
+            ArrayAdapter(this, android.R.layout.simple_list_item_1, listOfCollections)
         list.adapter = arrayAdapter
 
+
+        val btn: Button = findViewById(R.id.btn2)
+
+        btn.setOnClickListener(View.OnClickListener {
+
+        })
 
         fab.setOnClickListener(View.OnClickListener {
             val intent = Intent(this, CreateCollectionActivity::class.java)
@@ -56,6 +79,5 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
-
 
 }
