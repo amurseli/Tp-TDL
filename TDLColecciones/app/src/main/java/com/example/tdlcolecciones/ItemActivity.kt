0f8249ed858a10f582ce919c5fb2b3ae2db0dba2
MateoF01 <mateofernandez380@gmail.com>
2022-06-22 20:14:32 -0300

    cambio estetico ITEM

--- TDLColecciones/app/src/main/java/com/example/tdlcolecciones/ItemActivity.kt
+++ TDLColecciones/app/src/main/java/com/example/tdlcolecciones/ItemActivity.kt
@@ -1,13 +1,19 @@
 package com.example.tdlcolecciones
 
+import android.graphics.Color
 import android.graphics.Typeface
 import android.os.Bundle
+import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.marginLeft
 
 class ItemActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item)
@@ -21,13 +27,22 @@ class ItemActivity : AppCompatActivity() {
         actionBar!!.title = item.name
         actionBar.setDisplayHomeAsUpEnabled(true)
 
+        val typeface = ResourcesCompat.getFont(this.applicationContext, R.font.google_sans_regular)
+        val tamanioText = 27F
+
+
+
+
         for ((key,value) in item.dictionaryOfAttribute){
-            var txt_dinamic = TextView(this)
-            var tamanio_text = 27F
-            txt_dinamic.setTypeface(null, Typeface.BOLD)
-            txt_dinamic.marginLeft
-            txt_dinamic.textSize = tamanio_text
+
+            val txt_dinamic = TextView(this)
+            txt_dinamic.typeface = typeface
+            txt_dinamic.setTextColor(Color.BLACK)
+            txt_dinamic.textAlignment = View.TEXT_ALIGNMENT_CENTER
+            txt_dinamic.setBackgroundResource(R.drawable.gradient_1)
+            txt_dinamic.textSize = tamanioText
             txt_dinamic.text = "${key} : ${value}"
+
             columnaVertical.addView(txt_dinamic)
         }
 
